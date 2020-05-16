e553575db128561ca3be7831f9a494fdf683fa02
Ramon Domingos <ramongemio@gmail.com>
2020-05-16 19:03:40 -0300

    Update UserController.js
    observacoes sobe secret_jwt

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -2,6 +2,9 @@ const User = require('../models/user')
 const bcrypt = require('bcryptjs');
 const { check, validationResult } = require('express-validator');
 var jwt = require('jsonwebtoken');
+/* O ideal, é que assim como as senhas a secret esteja em uma variavel de ambiente, e nao diretamente no código,
+* Mas, para fins de testes e verificaçoes no jwt.io, definirei a secret_jwt abaixo:
+*/
 const secret_jwt = `lkajsdkasdkajsd,`
 const salt = bcrypt.genSaltSync(10);
 
