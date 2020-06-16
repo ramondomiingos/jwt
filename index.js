98d8acf879d1a4deb968ddd43316c073dee5c063
Ramon Domingos <ramongemio@gmail.com>
2020-06-15 23:17:30 -0300

    add index

---
+++ index.js
@@ -0,0 +1,20 @@
+var http = require('http');
+const express = require('express')
+const mongoose = require('mongoose')
+const routes = require('./routes')
+const app = express();
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+mongoose.connect('mongodb+srv://user:password@cluster0-lk5dw.mongodb.net/test?retryWrites=true&w=majority', {
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
+})
+app.get(`/`, (req, res)=>{
+    return res.json({'oi':'Hello'})
+});
+app.use(routes);
+
+app.listen(process.env.PORT);
